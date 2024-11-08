755e7b9bbd8041f3ccc3e9bb9d2f15b6cff39db7
jhyunleehi <jhyunleehi@naver.com>
2024-11-08 10:28:39 +0900

    reset

--- trace/nodeGraph.go
+++ trace/nodeGraph.go
@@ -7,49 +7,11 @@ import (
 	"strings"
 	"time"
 
-	nested "github.com/antonfisher/nested-logrus-formatter"
-	"github.com/prometheus/client_golang/prometheus"
+	nested "github.com/antonfisher/nested-logrus-formatter"	
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	NodeMetric = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "graph_node",
-			Help: "Node data for Grafana's Node Graph",
-		},
-		[]string{
-			"id",            //Unique identifier of the node. This ID is referenced by edge in its source and target field.
-			"title",         //Name of the node visible in just under the node.
-			"mainStat",      //First stat shown inside the node itself
-			"secondaryStat", //Same as mainStat, but shown under it inside the node
-			"arc__failed",   //to create the color circle around the node. All values in these fields should add up to 1.
-			"arc__passed",
-			"detail__role", //shown in the header of context menu when clicked on the node
-			"color",        //Can be used to specify a single color instead of using the arc__ fields to specify color sections
-			"icon",
-			"nodeRadius",  //Radius value in pixels. Used to manage node size.
-			"highlighted", //Sets whether the node should be highlighted.
-		},
-	)
-
-	EdgeMetric = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "graph_edge",
-			Help: "Edge relationship data for Grafana's Node Graph",
-		},
-		[]string{
-			"id",            //Unique identifier of the edge.
-			"source",        //Id of the source node.
-			"target",        //Id of the target.
-			"mainStat",      //First stat shown in the overlay when hovering over the edge.
-			"secondarystat", //Same as mainStat, but shown right under it.
-			"detail__info",  //will be shown in the header of context menu when clicked on the edge
-			"thickness",     //The thickness of the edge. Default: 1
-			"highlighted",   //boolean	Sets whether the edge should be highlighted.
-			"color",         //string	Sets the default color of the edge. It can be an acceptable HTML color string. Default: #999
-		},
-	)
 )
 
 func init() {
@@ -68,9 +30,7 @@ func init() {
 		},
 	})
 
-	// Prometheus에 메트릭 등록
-	prometheus.MustRegister(NodeMetric)
-	prometheus.MustRegister(EdgeMetric)
+
 }
 
 func (t *Trace) CreateNodeGraph() error {
@@ -125,6 +85,9 @@ func (t *Trace) CreateNodeGraph() error {
 }
 
 func (t *Trace) CreatePrometheusMetric() (err error) {
+	
+	t.NodeMetric.Reset()
+	t.EdgeMetric.Reset()
 
 	t.createNodeHost()
 	t.createNodePid()
@@ -141,38 +104,38 @@ func (t *Trace) CreatePrometheusMetric() (err error) {
 }
 
 func (t *Trace) createNodeHost() (err error) {
-	nodeId := fmt.Sprintf("%s", t.Hostname)
+	nodeId := fmt.Sprintf("host:%s", t.Hostname)
 	nodeName := fmt.Sprintf("host:%s", t.Hostname)
 	nodeMainStat := t.targetCommnd
 	nodeSubStat := fmt.Sprintf("%d", 0)
 	nodeArcFail := fmt.Sprintf("%f", 0.0)
 	nodeArcPass := fmt.Sprintf("%f", 1.0)
 	nodeRole := ""
-	nodeColor := "blue"
+	nodeColor := "green"
 	nodeIcon := ""
 	nodeRadius := ""
 	nodeHighlighted := "true"
 	newNode := []string{nodeId, nodeName, nodeMainStat, nodeSubStat, nodeArcFail, nodeArcPass, nodeRole, nodeColor, nodeIcon, nodeRadius, nodeHighlighted}
-	NodeMetric.WithLabelValues(newNode...).Set(1)
+	t.NodeMetric.WithLabelValues(newNode...).Set(1)
 	return nil
 }
 
 func (t *Trace) createNodePid() (err error) {
 	//pid
 	for _, pid := range t.Pid {
-		nodeId := fmt.Sprintf("%s:%d", t.Hostname, pid)
+		nodeId := fmt.Sprintf("pid:%s:%d", t.Hostname, pid)
 		nodeName := fmt.Sprintf("pid:%d", pid)
 		nodeMainStat := "running"
 		nodeSubStat := fmt.Sprintf("%d", t.Io[pid].ReadIos+t.Io[pid].WriteIos)
 		nodeArcFail := fmt.Sprintf("%f", 0.0)
 		nodeArcPass := fmt.Sprintf("%f", 1.0)
 		nodeRole := ""
-		nodeColor := ""
+		nodeColor := "blue"
 		nodeIcon := ""
 		nodeRadius := ""
 		nodeHighlighted := ""
 		newNode := []string{nodeId, nodeName, nodeMainStat, nodeSubStat, nodeArcFail, nodeArcPass, nodeRole, nodeColor, nodeIcon, nodeRadius, nodeHighlighted}
-		NodeMetric.WithLabelValues(newNode...).Set(1)
+		t.NodeMetric.WithLabelValues(newNode...).Set(1)
 	}
 	return nil
 }
@@ -180,19 +143,19 @@ func (t *Trace) createNodePid() (err error) {
 func (t *Trace) createNodePidFd() (err error) {
 	for _, pid := range t.Pid {
 		for _, fd := range t.Fd[pid] {
-			nodeId := fmt.Sprintf("%s:%d:%s", t.Hostname, pid, fd.Id)
+			nodeId := fmt.Sprintf("fd:%s:%d:%s", t.Hostname, pid, fd.Id)
 			nodeName := fmt.Sprintf("fd:%s", fd.Path)
 			nodeMainStat := "open"
 			nodeSubStat := fmt.Sprintf("%d MiB", fd.Size/1024/12024) //size
 			nodeArcFail := fmt.Sprintf("%f", 0.0)
-			nodeArcPass := fmt.Sprintf("%f", 1.0)
+			nodeArcPass := fmt.Sprintf("%f", 0.0)
 			nodeRole := ""
 			nodeColor := ""
-			nodeIcon := ""
+			nodeIcon := "file-alt"
 			nodeRadius := ""
 			nodeHighlighted := ""
 			newNode := []string{nodeId, nodeName, nodeMainStat, nodeSubStat, nodeArcFail, nodeArcPass, nodeRole, nodeColor, nodeIcon, nodeRadius, nodeHighlighted}
-			NodeMetric.WithLabelValues(newNode...).Set(1)
+			t.NodeMetric.WithLabelValues(newNode...).Set(1)
 		}
 	}
 	return nil
@@ -202,7 +165,7 @@ func (t *Trace) createNodeFS() (err error) {
 
 	for mountPoint := range t.Fs {
 		fs := t.FileSystemMap[mountPoint]
-		nodeId := fmt.Sprintf("%s:%s", t.Hostname, fs.MountPoint)
+		nodeId := fmt.Sprintf("fs:%s:%s", t.Hostname, fs.MountPoint)
 		nodeName := fs.MountPoint
 		nodeMainStat := fs.Type
 		nodeSubStat := fmt.Sprintf("%d MiB", 0) //size
@@ -214,7 +177,7 @@ func (t *Trace) createNodeFS() (err error) {
 		nodeRadius := ""
 		nodeHighlighted := "true"
 		newNode := []string{nodeId, nodeName, nodeMainStat, nodeSubStat, nodeArcFail, nodeArcPass, nodeRole, nodeColor, nodeIcon, nodeRadius, nodeHighlighted}
-		NodeMetric.WithLabelValues(newNode...).Set(1)
+		t.NodeMetric.WithLabelValues(newNode...).Set(1)
 	}
 	return nil
 }
@@ -222,9 +185,9 @@ func (t *Trace) createNodeFS() (err error) {
 func (t *Trace) createNodeDevice() (err error) {
 	for k, ds := range t.Dev {
 		devicePath := t.DeviceMap[k]
-		nodeId := fmt.Sprintf("%s:%s", t.Hostname, devicePath)
+		nodeId := fmt.Sprintf("dev:%s:%s", t.Hostname, devicePath)
 		nodeName := devicePath
-		nodeMainStat := "oline"
+		nodeMainStat := "online"
 		nodeSubStat := fmt.Sprintf("%d MiB", ds.Size/1024/1024) //size
 		nodeArcFail := fmt.Sprintf("%f", 0.0)
 		nodeArcPass := fmt.Sprintf("%f", 1.0)
@@ -234,7 +197,7 @@ func (t *Trace) createNodeDevice() (err error) {
 		nodeRadius := ""
 		nodeHighlighted := ""
 		newNode := []string{nodeId, nodeName, nodeMainStat, nodeSubStat, nodeArcFail, nodeArcPass, nodeRole, nodeColor, nodeIcon, nodeRadius, nodeHighlighted}
-		NodeMetric.WithLabelValues(newNode...).Set(1)
+		t.NodeMetric.WithLabelValues(newNode...).Set(1)
 	}
 	return nil
 }
@@ -242,8 +205,8 @@ func (t *Trace) createNodeDevice() (err error) {
 func (t *Trace) createEdgeHostPid() (err error) {
 	for _, pid := range t.Pid {
 		edgeId := fmt.Sprintf("%s:%d", t.Hostname, pid)
-		edgeSource := fmt.Sprintf("%s", t.Hostname)
-		edgeTarget := fmt.Sprintf("%s:%d", t.Hostname, pid)
+		edgeSource := fmt.Sprintf("host:%s", t.Hostname)
+		edgeTarget := fmt.Sprintf("pid:%s:%d", t.Hostname, pid)
 		edgeMainStat := "open"
 		edgeSecondarystat := "online"
 		edgeDetail__info := ""
@@ -251,7 +214,7 @@ func (t *Trace) createEdgeHostPid() (err error) {
 		edgeHighlighted := ""
 		edgeColor := ""
 		newNode := []string{edgeId, edgeSource, edgeTarget, edgeMainStat, edgeSecondarystat, edgeDetail__info, edgeThickness, edgeHighlighted, edgeColor}
-		EdgeMetric.WithLabelValues(newNode...).Set(1)
+		t.EdgeMetric.WithLabelValues(newNode...).Set(1)
 	}
 	return nil
 }
@@ -260,8 +223,8 @@ func (t *Trace) createEdgePidFd() (err error) {
 	for _, pid := range t.Pid {
 		for _, fd := range t.Fd[pid] {
 			edgeId := fmt.Sprintf("%s:%d:%s", t.Hostname, pid, fd.Id)
-			edgeSource := fmt.Sprintf("%s:%d", t.Hostname, pid)
-			edgeTarget := fmt.Sprintf("%s:%d:%s", t.Hostname, pid, fd.Id)
+			edgeSource := fmt.Sprintf("pid:%s:%d", t.Hostname, pid)
+			edgeTarget := fmt.Sprintf("fd:%s:%d:%s", t.Hostname, pid, fd.Id)
 			edgeMainStat := "open"
 			edgeSecondarystat := "online"
 			edgeDetail__info := ""
@@ -269,7 +232,7 @@ func (t *Trace) createEdgePidFd() (err error) {
 			edgeHighlighted := ""
 			edgeColor := ""
 			newNode := []string{edgeId, edgeSource, edgeTarget, edgeMainStat, edgeSecondarystat, edgeDetail__info, edgeThickness, edgeHighlighted, edgeColor}
-			EdgeMetric.WithLabelValues(newNode...).Set(1)
+			t.EdgeMetric.WithLabelValues(newNode...).Set(1)
 		}
 	}
 	return nil
@@ -279,9 +242,8 @@ func (t *Trace) createEdgFdFs() (err error) {
 	for _, pid := range t.Pid {
 		for _, fd := range t.Fd[pid] {
 			edgeId := fmt.Sprintf("%s:%d:%s", t.Hostname, pid, fd.Id)
-			edgeSource := fmt.Sprintf("%s:%d:%s", t.Hostname, pid, fd.Id)
-			//mountPoint :=fd.MountPoint
-			edgeTarget := fmt.Sprintf("%s:%s", t.Hostname, fd.MountPoint)
+			edgeSource := fmt.Sprintf("fd:%s:%d:%s", t.Hostname, pid, fd.Id)			
+			edgeTarget := fmt.Sprintf("fs:%s:%s", t.Hostname, fd.MountPoint)
 			edgeMainStat := "open"
 			edgeSecondarystat := "online"
 			edgeDetail__info := ""
@@ -289,19 +251,19 @@ func (t *Trace) createEdgFdFs() (err error) {
 			edgeHighlighted := ""
 			edgeColor := ""
 			newNode := []string{edgeId, edgeSource, edgeTarget, edgeMainStat, edgeSecondarystat, edgeDetail__info, edgeThickness, edgeHighlighted, edgeColor}
-			EdgeMetric.WithLabelValues(newNode...).Set(1)
+			t.EdgeMetric.WithLabelValues(newNode...).Set(1)
 		}
 	}
 	return nil
 }
 
-func (t *Trace) createEdgFsDevice() (err error) {
+func (t *Trace) createEdgFsDevice() (err error) {	
 	for _, fs := range t.Fs {
 		edgeId := fmt.Sprintf("%s:%s", t.Hostname, fs.MountPoint)
-		edgeSource := fmt.Sprintf("%s:%s", t.Hostname, fs.MountPoint)
+		edgeSource := fmt.Sprintf("fs:%s:%s", t.Hostname, fs.MountPoint)
 		deviceNumber := fs.DeviceNumber
 		dv := t.DeviceMap[deviceNumber]
-		edgeTarget := fmt.Sprintf("%s:%s", t.Hostname, dv)
+		edgeTarget := fmt.Sprintf("dev:%s:%s", t.Hostname, dv)
 		edgeMainStat := "open"
 		edgeSecondarystat := "online"
 		edgeDetail__info := ""
@@ -309,7 +271,7 @@ func (t *Trace) createEdgFsDevice() (err error) {
 		edgeHighlighted := ""
 		edgeColor := ""
 		newNode := []string{edgeId, edgeSource, edgeTarget, edgeMainStat, edgeSecondarystat, edgeDetail__info, edgeThickness, edgeHighlighted, edgeColor}
-		EdgeMetric.WithLabelValues(newNode...).Set(1)
+		t.EdgeMetric.WithLabelValues(newNode...).Set(1)
 	}
 	return nil
 }
